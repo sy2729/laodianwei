73e941b35d79252642c301badfdbfa5abd3705f5
sy2729 <sy2729@tc.columbia.edu>
2019-02-05 16:13:54 +0800

    build v1.3 switch to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Home from './views/Home';
 import Footer from './components/Footer';
 import About from './views/About';
